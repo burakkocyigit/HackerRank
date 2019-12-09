d5290d2a693665c09f6b59ec3f56b48b14f3c2e4
burak koçyiğit <burak-kocyigit@hotmail.com>
2019-12-09 08:58:18 +0300

    09/12/2019

---
+++ 20190425_HackerRank/ConsoleApp6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            staircase(n);
+        }
+        static void staircase(int n)
+        {
+            string stairCase = string.Empty;
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if ((i + j) < (n - 1))
+                        stairCase += " ";
+                    else
+                        stairCase += "#";
+                }
+                Console.WriteLine(stairCase);
+                stairCase = string.Empty;
+            }
+            Console.ReadLine();
+        }
+    }
+}
