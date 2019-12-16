044d52cde00603f0151bb08409dea17520786325
burak koçyiğit <burak-kocyigit@hotmail.com>
2019-12-16 10:26:11 +0300

    16/12/2019

---
+++ 20190425_HackerRank/ConsoleApp8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(System.Reflection.Assembly.GetExecutingAssembly().CodeBase.Remove(0, 8).Remove(95) + "input.txt", true);
+
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+            ;
+            int result = birthdayCakeCandles(ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+        static int birthdayCakeCandles(int[] ar)
+        {
+            Array.Sort(ar);
+            for (int i = ar.Length - 1; i > 0; i--)
+            {
+                if (ar[i] != ar[i - 1])
+                {
+                    return ar.Length - i;
+                }
+            }
+            return ar.Length;
+        }
+    }
+}
